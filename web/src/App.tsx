a3ae17de4a977b95085198c3bf1c8a735a1ec55a
Raphael Rodrigues <rars.student@gmail.com>
2022-09-13 20:27:30 -0300

    ADD mobile project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,19 @@
-
-function App() {
-  return <h1> Hello React </h1>
+interface ButtonProps {
+  title: string;
 }
 
-export default App
+function Button(props: ButtonProps) {
+  return <button>{props.title}</button>;
+}
+
+function App() {
+  return (
+    <div className="btn-group">
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+    </div>
+  );
+}
+
+export default App;
