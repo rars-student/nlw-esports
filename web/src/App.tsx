0a9f06c4e814ee91275115880b58799eb00f466a
Raphael Rodrigues <rars.developer@gmail.com>
2022-09-16 13:12:35 -0300

    Estilizando página dos jogos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,8 +3,9 @@ import "./styles/main.css";
 import logoImg from "./assets/logo-nlw-esports@3x.png";
 function App() {
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
-      <img src={logoImg} alt="" />
+    <div className="max-w-[344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="Logo eSports" />
+      <h1 className="text-6x1 text-white font-black mt-20">Seu duo esta aqui</h1>
     </div>
   );
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {
+      colors: {
+        nlw: "linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)",
+      },
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
       },
